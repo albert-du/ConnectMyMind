8ff151de22f32c704ce95ff94fa1bc98ed185eb7
Alexander Bakalov <alexander.i.bakalov@gmail.com>
2022-05-21 12:22:02 -0700

    creating sample app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http'),
+    fs = require('fs'),
+    path = require('path');
+
+const hostname = '34.82.23.76';
+const port = 80;
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+    const surl = new URL(url, 'http://192.168.1.23:8001/');
+    if (method == 'GET' && surl.pathname == '/') {
+        fs.readFile('./client/connect-my-mind/src/app.js', function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(html);
+            res.end();
+        });
+        return;
+    }
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 404;
+    res.end(`Not found1:`);
+    return;
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});

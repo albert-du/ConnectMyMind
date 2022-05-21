e7576be458890a82f3c3ee74cde517a9b9f753b4
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-21 15:24:50 -0700

    Setting up hosting for favicon and index.html

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const port = 80;
 const server = http.createServer((req, res) => {
     const { method, url } = req;
     const surl = new URL(url, 'http://connectmymind.ddns.net/');
+    if (method == 'GET' && surl.pathname == '/favicon.ico') {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/public/favicon.ico', function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+            res.write(html);
+            res.end();
+        });
+        return;
+    }
     if (method == 'GET' && surl.pathname == '/') {
-        console.log("h");
-        fs.readFile('./client/connect-my-mind/src/app.js', function (err, html) {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/public/index.html', function (err, html) {
             if (err) {
                 throw err;
             }

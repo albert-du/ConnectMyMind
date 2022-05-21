4529d1849e81d50c524c3c0a8dbd30a02e971c2d
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-21 15:04:17 -0700

    commenting for testing

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const hostname = '10.138.0.2';
 const port = 80;
 const server = http.createServer((req, res) => {
     const { method, url } = req;
-    const surl = new URL(url, 'http://connectmymind.ddns.net/');
-    if (method == 'GET' && surl.pathname == '/') {
-        fs.readFile('./client/connect-my-mind/src/app.js', function (err, html) {
-            if (err) {
-                throw err;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(html);
-            res.end();
-        });
-        return;
-    }
+    // const surl = new URL(url, 'http://connectmymind.ddns.net/');
+    // if (method == 'GET' && surl.pathname == '/') {
+    //     fs.readFile('./client/connect-my-mind/src/app.js', function (err, html) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         res.writeHead(200, { 'Content-Type': 'text/html' });
+    //         res.write(html);
+    //         res.end();
+    //     });
+    //     return;
+    // }
     res.setHeader('Content-Type', 'text/plain');
     res.statusCode = 404;
-    res.end(`Not found1:`);
+    res.end(`Not found:`);
     return;
 });
 

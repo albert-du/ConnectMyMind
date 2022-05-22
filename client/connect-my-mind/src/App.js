cd02c2346dd99e3566a454aebc31b3bb0fd1091a
AbhinavAkkiraju <abhinavakkiraju@gmail.com>
2022-05-21 23:35:15 -0700

    Working on home page

--- client/connect-my-mind/src/App.js
+++ client/connect-my-mind/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact component={Home} />
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </Router>
     </>
--- client/connect-my-mind/src/components/HomeSection.js
+++ client/connect-my-mind/src/components/HomeSection.js
@@ -3,16 +3,20 @@ import '../App.css';
 import { Button } from './Button';
 import './HomeSection.css';
 
-
 function HomeSection() {
     return (
-        <div className='hero-container'>
-            <video src="/client/connect-my-mind/src/components/brain.mp4" autoPlay loop muted />
+        <div className='home-container'>
+            <img src='/brain.gif'/>
             <h1>SEEK MENTAL HEALTH SUPPORT NOW</h1>
             <p>More convenient and easy than ever!</p>
             <div className="home-btns">
-                <Button className='btns' buttonStyle='btn--outline'
-                buttonSize='btn--large'>GET STARTED</Button>
+                <Button 
+                className='btns' 
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'
+                >
+                    GET STARTED
+                </Button>
             </div>
         </div>
     )

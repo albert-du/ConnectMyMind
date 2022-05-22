14622ca4d7579512b29952e9a8aaffe57996a732
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-22 02:51:00 -0700

    Revert "Working on sign up"
    This reverts commit 194b68de87882f83fb5649f540de21b18f5a5e9a.

--- client/connect-my-mind/src/App.js
+++ client/connect-my-mind/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Video from './components/Video.js';
 import Home from './components/pages/Home';
 import OnlineAppointments from './components/pages/OnlineAppointments';
-import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/onlineappointments' element={<OnlineAppointments/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
       </Routes>
     </Router>
     </>

2de38d939a79123712e05d7ab151c030c2e001fd
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-22 04:38:51 -0700

    adding textboxes

--- client/connect-my-mind/src/components/LocalSection.js
+++ client/connect-my-mind/src/components/LocalSection.js
@@ -6,8 +6,13 @@ import './LocalSection.css';
 function LocalSection() {
     return (
         <div className='local-container'>
-            <img src='/US-Blank-map.jpeg'/>
+            <img className='states' src='/US-Blank-map.jpeg'/>
             <h1>SELECT YOUR STATE</h1>
+            <input className='zipText' type="text" placeholder='Zip Code...'/>
+            <input className='zipText' type="text" placeholder='Range (Mi)...'/>
+            <button className='btn11'>
+                Search
+            </button>
         </div>
     )
 }

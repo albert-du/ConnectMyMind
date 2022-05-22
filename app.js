a7be3980febdddcbe30ba8b6ed7f785b8122d004
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-21 19:01:19 -0700

    Adding endpoint for static files

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const port = 80;
 const server = http.createServer((req, res) => {
     const { method, url } = req;
     const surl = new URL(url, 'http://connectmymind.ddns.net/');
+    if (method == 'GET' && surl.pathname.startsWith('/static')) {
+        let fileName = surl.pathname.substring(7);
+        let extention = fileName.substring(fileName.lastIndexOf('.') + 1);
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/build/static/' + extention + fileName, function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/' + extention });
+            res.write(html);
+            res.end();
+        });
+        return;
+    }
     if (method == 'GET' && surl.pathname == '/identity') {
         let searchParams = surl.searchParams;
         let user = searchParams.get('user');
@@ -16,7 +29,6 @@ const server = http.createServer((req, res) => {
         res.end();
         return;
     }
-
     if (method == 'GET' && surl.pathname == '/favicon.ico') {
         fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/public/favicon.ico', function (err, html) {
             if (err) {

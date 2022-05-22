0db7a9649c32edc10cc90b97f043ba74de4c056b
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-21 19:08:59 -0700

    adding endpoints for fontawesome.css and manifest.json

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const server = http.createServer((req, res) => {
         return;
     }
     if (method == 'GET' && surl.pathname == '/favicon.ico') {
-        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/public/favicon.ico', function (err, html) {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/build/favicon.ico', function (err, html) {
             if (err) {
                 throw err;
             }
@@ -41,11 +41,33 @@ const server = http.createServer((req, res) => {
         return;
     }
     if (method == 'GET' && surl.pathname == '/') {
-        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/public/index.html', function (err, html) {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/build/index.html', function (err, html) {
             if (err) {
                 throw err;
             }
             res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(html);
+            res.end();
+        });
+        return;
+    }
+    if (method == 'GET' && surl.pathname == '/manifest.json') {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/build/manifest.json', function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(html);
+            res.end();
+        });
+        return;
+    }
+    if (method == 'GET' && surl.pathname == '/fontawesome.css') {
+        fs.readFile('/home/alexander_i_bakalov/ConnectMyMind/ConnectMyMind/client/connect-my-mind/build/fontawesome.css', function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css' });
             res.write(html);
             res.end();
         });
